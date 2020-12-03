b1597592a0305de596d3deac5c6017517b887768
Shemona Singh <singhshemona@gmail.com>
2020-12-03 12:33:18 -0500

    set up audio request url and put it into an audio element with axios

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,12 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { setStatusBarNetworkActivityIndicatorVisible, StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { config } from '../../config.js';
 
-export default function App() {
+export const App = (): React.ReactElement => {
+  const [ audio, setAudio ] = useState('');
+  
+  useEffect(() => {
+    const options = {
+      url: 'https://listen-api.listennotes.com/api/v2/episodes/02f0123246c944e289ee2bb90804e41b',
+      method: 'GET',
+      headers: { 'X-ListenAPI-Key': config.KEY },
+    };
+
+    axios(options)
+      .then(response => {
+        console.log(response.data.audio)
+        setAudio(response.data.audio)
+      });
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <audio
+        controls
+        src={audio}
+      />
     </View>
   );
 }
